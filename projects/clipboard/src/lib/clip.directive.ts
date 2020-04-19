edc7cf1a94f5f3f79c578eae0e556a7ea30b0729
YuJM <polcaneli@gmail.com>
2020-04-19 23:52:41 +0900

    fix: clipboard 배포 빌드를 위한 수정

--- projects/clipboard/src/lib/clip.directive.ts
+++ projects/clipboard/src/lib/clip.directive.ts
@@ -1,55 +1,60 @@
-import { Directive, EventEmitter, HostListener, Inject, Input, OnInit, Output, Renderer2, ViewContainerRef } from '@angular/core';
+import {
+  Directive,
+  EventEmitter,
+  HostListener,
+  Inject,
+  Input,
+  Output,
+  Renderer2,
+  ViewContainerRef
+} from '@angular/core';
 import { Action, TargetTypes } from './types';
 import { DOCUMENT } from '@angular/common';
 
 @Directive({
-    selector: '[ngfClip]'
+	selector: '[ngfClip]'
 })
-export class ClipDirective implements OnInit {
-    @Input()ngfClip: TargetTypes;
-    @Input() action: Action;
-    @Output() ngfOut = new EventEmitter();
-    hostElement: any;
-    result: string;
+export class ClipDirective {
+	@Input() ngfClip: TargetTypes;
+	@Input() action: Action;
+	@Output() ngfOut = new EventEmitter();
+	hostElement: any;
+	result: string;
 
-    constructor(private viewContainerRef: ViewContainerRef,
-                @Inject(DOCUMENT) private document: Document,
-                private renderer: Renderer2) {
-        this.hostElement = this.viewContainerRef.element.nativeElement;
+	constructor(
+		private viewContainerRef: ViewContainerRef,
+		@Inject(DOCUMENT) private document: any,
+		private renderer: Renderer2
+	) {
+		this.hostElement = this.viewContainerRef.element.nativeElement;
+	}
 
-
-    }
-
-    ngOnInit(): void {
-
-    }
-
-    @HostListener('click', ['$event'])
-    hostClick($event) {
-        $event.preventDefault();
-        if (!!this.ngfClip) {
-            if (this.ngfClip instanceof Element) {
-                this.result = this.ngfClip.textContent;
-            } else if (typeof this.ngfClip === 'string') {
-                this.result = this.ngfClip;
-            }
-            const elem: HTMLTextAreaElement = this.document.createElement('textarea');
-            elem.value = this.result;
-            elem.style.fontSize = '12pt';
-            elem.style.border = '0';
-            elem.style.padding = '0';
-            elem.style.margin = '0';
-            elem.style.position = 'absolute';
-            elem.style.left = '-9999px'
-            elem.setAttribute('readonly', '');
-            this.renderer.appendChild(this.document.body, elem);
-            // this.document.body.appendChild(elem);
-            elem.focus();
-            elem.setSelectionRange(0, 9999);
-            this.document.execCommand('copy');
-            this.renderer.removeChild(this.document.body, elem);
-            this.ngfOut.next({copyText: this.result});
-            /*const cp = new clipboard(this.ngfClipboard);
+	@HostListener('click', ['$event'])
+	hostClick($event) {
+		$event.preventDefault();
+		if (!!this.ngfClip) {
+			if (this.ngfClip instanceof Element) {
+				this.result = this.ngfClip.textContent;
+			} else if (typeof this.ngfClip === 'string') {
+				this.result = this.ngfClip;
+			}
+			const elem: HTMLTextAreaElement = this.document.createElement('textarea');
+			elem.value = this.result;
+			elem.style.fontSize = '12pt';
+			elem.style.border = '0';
+			elem.style.padding = '0';
+			elem.style.margin = '0';
+			elem.style.position = 'absolute';
+			elem.style.left = '-9999px';
+			elem.setAttribute('readonly', '');
+			this.renderer.appendChild(this.document.body, elem);
+			// this.document.body.appendChild(elem);
+			elem.focus();
+			elem.setSelectionRange(0, 9999);
+			this.document.execCommand('copy');
+			this.renderer.removeChild(this.document.body, elem);
+			this.ngfOut.next({ copyText: this.result });
+			/*const cp = new clipboard(this.ngfClipboard);
             cp.on('success', (e) => {
                 console.log('Action:', e.action);
                 console.log('Text:', e.text);
@@ -58,7 +63,6 @@ export class ClipDirective implements OnInit {
 
                 e.clearSelection();
             });*/
-        }
-    }
-
+		}
+	}
 }
--- projects/clipboard/src/public-api.ts
+++ projects/clipboard/src/public-api.ts
@@ -3,6 +3,7 @@
  */
 
 export * from './lib/clipboard.service';
+export * from './lib/clip.directive';
 export * from './lib/clipboard.module';
 export * from './lib/interface';
 export * from './lib/types';
